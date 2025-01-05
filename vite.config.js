d5dfb2ae1684d00d702edb3bb03a872a14985e01
JDCodeJungler <136150816+Jaydee028@users.noreply.github.com>
2025-01-05 15:31:17 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import {fileURLToPath,URL} from 'node:url';
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portfolio/',
   plugins: [vue()],
-  resolve:{
-    alias:{
-      '@':fileURLToPath(new URL("./src",import.meta.url))
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL("./src", import.meta.url))
     }
   }
 })
